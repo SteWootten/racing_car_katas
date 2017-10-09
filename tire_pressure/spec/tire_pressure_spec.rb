d08119eb26b4177089c9c2b4974a75187cd0737e
Ste Wootten <stephen.wootten@onthebeach.co.uk>
2017-10-09 19:07:30 +0100

    Spec for when the pressure is too low
    Added a spec for when the pressure is too low and added contexts which
    allows me to assign a value to `sample_pressure` in each context. This
    allows me to easily change the value returned by
    `Sensor.sample_pressure` in each test

--- tire_pressure/spec/tire_pressure_spec.rb
+++ tire_pressure/spec/tire_pressure_spec.rb
@@ -5,12 +5,24 @@ RSpec.describe Alarm do
   describe '#check' do
 
     before do
-      allow(Sensor).to receive(:sample_pressure).and_return(20)
+      allow(Sensor).to receive(:sample_pressure).and_return(sample_pressure)
       subject.check
     end
 
-    it 'turns on the alarm when pressure is too high' do
-      expect(subject.is_alarm_on).to be true
+    context 'pressure is too high' do
+      let(:sample_pressure) { 20 }
+
+      it 'turns on the alarm when pressure is too high' do
+        expect(subject.is_alarm_on).to be true
+      end
+    end
+
+    context 'pressure is too low' do
+      let(:sample_pressure) { 0 }
+
+      it 'turns on the alarm when pressure is too low' do
+        expect(subject.is_alarm_on).to be true
+      end
     end
   end
 end
