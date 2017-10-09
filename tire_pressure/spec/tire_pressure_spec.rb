828bcb6db30448f4d0bf9d6af78047be67b8564e
Ste Wootten <stephen.wootten@onthebeach.co.uk>
2017-10-09 20:09:45 +0100

    Refactored to spec to describe what we are actually testing
    All the refactoring in the code base means we can tidy up the specs and
    use a lot of nice shorthand techniques. Also changed the `describe` to
    describe the method we are actually testing

--- tire_pressure/spec/tire_pressure_spec.rb
+++ tire_pressure/spec/tire_pressure_spec.rb
@@ -2,10 +2,11 @@ require_relative '../lib/tire_pressure'
 
 RSpec.describe Alarm do
 
-  describe '#check' do
-    subject { Alarm.new(sensor, acceptable_range) }
+  describe '#is_alarm_on' do
+    let(:subject) { alarm.is_alarm_on }
 
     let(:acceptable_range) { (17..21) }
+    let(:alarm) { Alarm.new(sensor, acceptable_range) }
     let(:sensor) {
       instance_double(
         'Sensor',
@@ -13,32 +14,22 @@ RSpec.describe Alarm do
       )
     }
 
-    before do
-      subject.check
-    end
-
     context 'pressure is too high' do
       let(:pressure) { 25 }
 
-      it 'turns on the alarm when pressure is too high' do
-        expect(subject.is_alarm_on).to be true
-      end
+      it { should be true }
     end
 
     context 'pressure is too low' do
       let(:pressure) { 0 }
 
-      it 'turns on the alarm when pressure is too low' do
-        expect(subject.is_alarm_on).to be true
-      end
+      it { should be true }
     end
 
     context 'pressure is OK' do
       let(:pressure) { 19 }
 
-      it 'does not turn on the alarm' do
-        expect(subject.is_alarm_on).to be false
-      end
+      it { should be false }
     end
   end
 end
